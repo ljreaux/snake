381e926f5c7b4ea40a432737adb29621deec5ba7
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-07-24 19:57:46 -0500

    Initial commit

---
+++ build/build.js
@@ -0,0 +1,11 @@
+function setup() {
+    console.log("🚀 - Setup initialized - P5 is running");
+    createCanvas(windowWidth, windowHeight);
+}
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+function draw() {
+    background(0);
+}
+//# sourceMappingURL=build.js.map
---
+++ src/sketch.ts
@@ -0,0 +1,17 @@
+// P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
+function setup() {
+  console.log("🚀 - Setup initialized - P5 is running");
+
+  createCanvas(windowWidth, windowHeight);
+}
+
+// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
+function draw() {
+  // CLEAR BACKGROUND
+  background(0);
+}
